51445910f1a6b509e0ab58909821622fcaad0b15
Julia Olausson <olaussonjulia@hotmail.com>
2019-12-10 11:14:05 +0100

    map funktion

--- script.js
+++ script.js
@@ -10,80 +10,15 @@
  * Method Chaining
  */
 
- /*
-const points = [42, 3, 16, 8, 84];
-const cats = [
-    'Miao',
-    'Megabite',
-    'C.A.T',
-    'William',
-    'Picatso'
-];
+ // map ()
 
-const cats = [
-    {name: 'Picatso', age: 8},
-    {name: 'Miao', age: 42},
-    {name: 'C.A.T', age: 3},
-    {name: 'William', age: 16},
-    {name: 'Megabite', age: 84},
-];
+ const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-const logCatName = catname => {
-    console.log(`Cat is called ${catname}`);
-}
+ const blackFridayPrices = prices.map(price => price / 2); // förkortad funktion när det endast är en parementer. 
+    //let newPrice = price / 2; 
+    //return newPrice;
 
-const ourOwnForEach = (arr, callback) => {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i]);
-    }
-};
-
-ourOwnForEach();
-
-//catnames.forEach(function(catname, i) {
-//    console.log(`Cat at index ${i} is ${catname}`);
-//});
-
-//catnames.forEach(logCatName);
-*/
-
-// FILTER
-
-/*
-const points = [42, 3, 16, 8, 84];
-
-//const filteredPoints = [];
-
-const filteredPoints = points.filter(function(point){
-    if (points > 20){
-        return true;
-    } else {
-        return false;
-    }
-});
-*/
-
-// filtrera alla katter som är äldre än 7 år. 
-
-const cats = [
-    {name: 'Picatso', age: 8},
-    {name: 'Miao', age: 42},
-    {name: 'C.A.T', age: 3},
-    {name: 'William', age: 4},
-    {name: 'Megabite', age: 84},
-];
-
-const seniorCats = cats.filter(cat => cat.age >= 7); // Nedkortad arrow function. Behöver inte skriva return.
-    //return cat.age >= 7; 
-
-
-
-
-
-
-
-
-
+ console.log('Black friday prices is', blackFridayPrices);
 
 
 
